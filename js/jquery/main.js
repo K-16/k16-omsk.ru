b336b4fdc89b8d1db4845143f7da450d8ad560a8
Senya Pugach <upisfree@ya.ru>
2012-12-16 20:10:42 +0400

    Update gallery

--- js/jquery/main.js
+++ js/jquery/main.js
@@ -1,28 +1,22 @@
 $(document).ready(function()
 {
 
- $('#logo').click(function()      {$('title').text('Типографика | К16'); $.scrollTo(0, 500);});
+ $("#logo").click(function()      {$("title").text("Типографика | К16"); $.scrollTo(0, 500);});
  
- $('.control-1').click(function() {$('title').text('Типографика | К16'); $.scrollTo('#panel-1', 500, {axis:'y'});});
- $('.control-2').click(function() {$('title').text('Секстант | К16'); $.scrollTo('#panel-2', 500, {axis:'y'});});
- $('.control-3').click(function() {$('title').text('Конфликт | К16'); $.scrollTo('#panel-3', 500, {axis:'y'});});
- $('.control-4').click(function() {$('title').text('Поясность | К16'); $.scrollTo('#panel-4', 500, {axis:'y'});});
- $('.control-5').click(function() {$('title').text('Галерея | К16'); $.scrollTo('#panel-5', 500, {axis:'y'});});
+ $(".control-1").click(function() {$("title").text("Типографика | К16"); $.scrollTo("#panel-1", 500, {axis:"y"});});
+ $(".control-2").click(function() {$("title").text("Секстант | К16"); $.scrollTo("#panel-2", 500, {axis:"y"});});
+ $(".control-3").click(function() {$("title").text("Конфликт | К16"); $.scrollTo("#panel-3", 500, {axis:"y"});});
+ $(".control-4").click(function() {$("title").text("Поясность | К16"); $.scrollTo("#panel-4", 500, {axis:"y"});});
+ $(".control-5").click(function() {$("title").text("Галерея | К16"); $.scrollTo("#panel-5", 500, {axis:"y"});});
  
- $('#fpc').click(function() {$.scrollTo('#fp', 500, {axis:'x'});});
- $('#vpc').click(function() {$.scrollTo('#vp', 500, {axis:'x'});});
-
- $('#gc-1').click(function() {$.scrollTo('#g-1', 500, {axis:'x'});});
- $('#gc-2').click(function() {$.scrollTo('#g-2', 500, {axis:'x'});});
- $('#gc-3').click(function() {$.scrollTo('#g-3', 500, {axis:'x'});});
+ $("#fpc").click(function() {$.scrollTo("#fp", 500, {axis:"x"});});
+ $("#vpc").click(function() {$.scrollTo("#vp", 500, {axis:"x"});});
  
- $('.gback').click(function() {$.scrollTo(0, 500, {axis:'x'});});
+ $(".gback").click(function() {$.scrollTo(0, 500, {axis:"x"});});
  
- $('#fback').click(function() {$.scrollTo('#f', 500, {axis:'x'});});
- 
- 
+ $("#fback").click(function() {$.scrollTo("#f", 500, {axis:"x"});});
   
- $('.g').fotorama();
+ $(".g").fotorama();
  
 //Hide menu for button "Back" in galleries
  $(function () 
@@ -31,11 +25,11 @@ $(document).ready(function()
   {
 	 if ($(this).scrollLeft() > 500 ) 
    {
-		$('#left').fadeOut(1);
+		$("#left").fadeOut(1);
 	 } 
    else 
     {
-		 $('#left').fadeIn(1);
+		 $("#left").fadeIn(1);
 		}
 	});  
  });
@@ -49,5 +43,3 @@ fotoramaDefaults =
   fullscreen: true,
   preload: 1
 }
- 
-
