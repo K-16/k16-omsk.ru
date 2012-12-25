98c122231bb90dc3833b9eceaaf4ad34f9a62f69
Senya Pugach <upisfree@ya.ru>
2012-12-25 19:45:49 +0400

    New menu, more good gallery, many little problems

--- js/jquery/main.js
+++ js/jquery/main.js
@@ -1,35 +1,56 @@
 $(document).ready(function()
 {
 
- $("#logo").click(function()      {$("title").text("Типографика | К16"); $.scrollTo(0, 500);});
+// Menu array
+
+ $('#gototop').click(function()   {$('.ctrl').css(ctrlUnCheck); $('#ctrl-1').css(ctrlCheck); $('title').text('Типографика | К16'); $.scrollTo(0, 500);});
  
- $(".control-1").click(function() {$("title").text("Типографика | К16"); $.scrollTo("#panel-1", 500, {axis:"y"});});
- $(".control-2").click(function() {$("title").text("Секстант | К16"); $.scrollTo("#panel-2", 500, {axis:"y"});});
- $(".control-3").click(function() {$("title").text("Конфликт | К16"); $.scrollTo("#panel-3", 500, {axis:"y"});});
- $(".control-4").click(function() {$("title").text("Поясность | К16"); $.scrollTo("#panel-4", 500, {axis:"y"});});
- $(".control-5").click(function() {$("title").text("Галерея | К16"); $.scrollTo("#panel-5", 500, {axis:"y"});});
+ $('#ctrl-1').click(function() {$('.ctrl').css(ctrlUnCheck); $('#ctrl-1').css(ctrlCheck); $.scrollTo('#panel-1', 500, {axis:'y'}); $('title').text('Типографика | К16');});
+ $('#ctrl-2').click(function() {$('.ctrl').css(ctrlUnCheck); $('#ctrl-2').css(ctrlCheck); $.scrollTo('#panel-2', 500, {axis:'y'}); $('title').text('Секстант | К16');});
+ $('#ctrl-3').click(function() {$('.ctrl').css(ctrlUnCheck); $('#ctrl-3').css(ctrlCheck); $.scrollTo('#panel-3', 500, {axis:'y'}); $('title').text('Конфликт | К16');});
+ $('#ctrl-4').click(function() {$('.ctrl').css(ctrlUnCheck); $('#ctrl-4').css(ctrlCheck); $.scrollTo('#panel-4', 500, {axis:'y'}); $('title').text('Поясность | К16');});
+ $('#ctrl-5').click(function() {$('.ctrl').css(ctrlUnCheck); $('#ctrl-5').css(ctrlCheck); $.scrollTo('#panel-5', 500, {axis:'y'}); $('title').text('Галерея | К16');});
  
- $("#fpc").click(function() {$.scrollTo("#fp", 500, {axis:"x"});});
- $("#vpc").click(function() {$.scrollTo("#vp", 500, {axis:"x"});});
+ var ctrlCheck = 
+ {
+   'background-color' : '#FFFFFF',
+   'color' : '#1a4780',
+   'text-shadow' : '0px 0px 1px #1a4780',
+ }
+
+ var ctrlUnCheck =
+ {
+   'background-color' : '#4682b4',
+   'color' : '#fff',
+   'text-shadow' : '0px 0px 2px #fff',
+ }
+
+ $('#fpc').click(function() {$.scrollTo('#fp', 500, {axis:'x'});});
+ $('#vpc').click(function() {$.scrollTo('#vp', 500, {axis:'x'});});
  
- $(".gback").click(function() {$.scrollTo(0, 500, {axis:"x"});});
+ $('.gback').click(function() {$.scrollTo(0, 500, {axis:'x'});});
  
- $("#fback").click(function() {$.scrollTo("#f", 500, {axis:"x"});});
+ $('#fback').click(function() {$.scrollTo('#f', 500, {axis:'x'});});
   
- $(".g").fotorama();
+ $('.g').fotorama();
+
+
+// Smoothly animate array
+
+ $('.gpa, .gback, #fback, #logo, .ctrl, #gototop').removeAttr('href');
  
-//Hide menu for button "Back" in galleries
+//Hide menu for button 'Back' in galleries
  $(function () 
  {
 	$(window).scroll(function () 
   {
 	 if ($(this).scrollLeft() > 500 ) 
    {
-		$("#left").fadeOut(1);
+		$('#left').fadeOut(1);
 	 } 
    else 
     {
-		 $("#left").fadeIn(1);
+		 $('#left').fadeIn(1);
 		}
 	});  
  });
@@ -41,5 +62,6 @@ $(document).ready(function()
 fotoramaDefaults = 
 {
   fullscreen: true,
-  preload: 1
+  preload: 1,
+//  nav: 'dots'
 }
--- pic/picmas.js
+++ pic/picmas.js
@@ -1,69 +1,48 @@
 $(document).ready(function()
 {
 
-  $("#gc-1").click(function() 
-  {
-    $.scrollTo("#g-1", 500, {axis:"x"});
-    $("#g-1").fotorama
-    ({
-      data: 
-      [
-        {img: "http://www.bugaga.ru/uploads/posts/2009-01/1231616841_rambutan.jpg"},
-        {img: "http://www.bugaga.ru/uploads/posts/2009-01/1231616686_longan.jpg"},
-        {img: "http://nevseoboi.com.ua/uploads/posts/2010-02/1267200702_042.jpg"},
-      ]
-    });
+// ScrollTo array
+
+  $('#gc-1').click(function() {$.scrollTo('#g-1', 500, {axis:'x'});});
+  $('#gc-2').click(function() {$.scrollTo('#g-2', 500, {axis:'x'});});
+  $('#gc-3').click(function() {$.scrollTo('#g-3', 500, {axis:'x'});});
+
+// Pics array
+
+  $('#g-1').fotorama
+  ({
+    data: 
+    [
+      {img: 'http://cs317931.userapi.com/v317931374/5163/VaC9lnz_F5U.jpg'},
+      {img: 'http://cs317931.userapi.com/v317931374/5190/uitQGMv1Gjk.jpg'},
+      {img: 'http://cs317931.userapi.com/v317931374/5187/NIC_mPkDxCM.jpg'},
+      {img: 'http://cs317931.userapi.com/v317931374/517e/N8Imvi-KquU.jpg'},
+      {img: 'http://cs317931.userapi.com/v317931374/5175/fPzuQa49h_U.jpg'},
+      {img: 'http://cs317931.userapi.com/v317931374/516c/CH4E0gX5Zls.jpg'},
+    ]
   });
 
-  $("#gc-2").click(function() 
-  {
-    $.scrollTo("#g-2", 500, {axis:"x"});
-    $("#g-2").fotorama
-    ({
-      data: 
-      [
-        {img: "http://gamelika.com/imaginator/1/4e5fd85c58b8e_fru.jpg"},
-        {img: "http://www.dezinfo.net/images2/image/06.2009/tropiowosi/1009.jpg"},
-        {img: "http://i030.radikal.ru/0712/c5/498a5c90db25.jpg"},
-      ]
-    });
+
+  $('#g-2').fotorama
+  ({
+    data: 
+    [
+      {img: 'http://gamelika.com/imaginator/1/4e5fd85c58b8e_fru.jpg'},
+      {img: 'http://www.dezinfo.net/images2/image/06.2009/tropiowosi/1009.jpg'},
+      {img: 'http://i030.radikal.ru/0712/c5/498a5c90db25.jpg'},
+    ]
   });
 
-  $("#gc-3").click(function() 
-  {
-    $.scrollTo("#g-3", 500, {axis:"x"});
-    $("#g-3").fotorama
-    ({
-      data: 
-      [
-        {img: "http://img-fotki.yandex.ru/get/4514/val-pavlovna.14b/0_70297_fba0f03b_L"},
-        {img: "http://totalbeautyglamour.ru/images/pages/fill/h600/67a8fe8f6595bbc02bbbd59b8a3c825e.jpg"},
-        {img: "http://www.edabezvreda.ru/pict/407.jpg"},
-      ]
-    });
+
+  $('#g-3').fotorama
+  ({
+    data: 
+    [
+      {img: 'http://img-fotki.yandex.ru/get/4514/val-pavlovna.14b/0_70297_fba0f03b_L'},
+      {img: 'http://totalbeautyglamour.ru/images/pages/fill/h600/67a8fe8f6595bbc02bbbd59b8a3c825e.jpg'},
+      {img: 'http://www.edabezvreda.ru/pict/407.jpg'},
+    ]
   });
+
 
 });
-/*
-<IMG src="http://www.bugaga.ru/uploads/posts/2009-01/1231616841_rambutan.jpg">
-<IMG src="http://www.bugaga.ru/uploads/posts/2009-01/1231616686_longan.jpg">
-<IMG src="http://nevseoboi.com.ua/uploads/posts/2010-02/1267200702_042.jpg">
-<IMG src="http://gamelika.com/imaginator/1/4e5fd85c58b8e_fru.jpg">
-<IMG src="http://www.dezinfo.net/images2/image/06.2009/tropiowosi/1009.jpg">
-<IMG src="http://i030.radikal.ru/0712/c5/498a5c90db25.jpg">
-<IMG src="http://img-fotki.yandex.ru/get/4514/val-pavlovna.14b/0_70297_fba0f03b_L">
-<IMG src="http://totalbeautyglamour.ru/images/pages/fill/h600/67a8fe8f6595bbc02bbbd59b8a3c825e.jpg">
-<IMG src="http://www.edabezvreda.ru/pict/407.jpg">
-<IMG src="http://img.galya.ru/galya.ru/Pictures2/themes_topics/2011/09/07/2777323.jpg">
-<IMG src="http://mens-belt.org/sites/default/files/01-1257687687574543654786.jpg">
-<IMG src="http://domaromatov.ru/published/publicdata/SOAPBUSIDASHOP/attachments/SC/products_pictures/index_enl.jpg">
-<IMG src="http://www.bluechameleon.org/Forum%20Pics%202005/-%20Lychee%20tree.jpg">
-<IMG src="http://images.reklama.com.ua/2011-10-31/1194920/photos0-800x600.jpeg">
-*/
-
-
-
-
-//  $("#gc-2").click(function() {$.scrollTo("#g-2", 500, {axis:"x"});});
-//  $("#gc-3").click(function() {$.scrollTo("#g-3", 500, {axis:"x"});});
-
