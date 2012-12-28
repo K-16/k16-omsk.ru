bed31b2ed49e49de9ab301264cf502d2b6a5f0ce
Senya Pugach <upisfree@ya.ru>
2012-12-28 19:11:25 +0600

    IE (1/2)

---
+++ js/jquery/iescript.js
@@ -0,0 +1,16 @@
+$(document).ready(function()
+{
+
+// Menu array
+
+ $('#gototop').click(function()   {$('.ctrl').css(ctrlUnCheck); $('#ctrl-1').css(ctrlCheck); $('title').text('Типографика | К16'); $.scrollTo(0, 500);});
+ $('.gpa, .gback, #fback, #logo, .ctrl, #gototop').attr('href');
+ 
+});
+
+
+fotoramaDefaults = 
+{
+  fullscreen: true,
+  preload: 1,
+}
