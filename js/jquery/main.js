2e793d41fd51460c9e1755e5d9eaa16fe2485abe
Senya Pugach <upisfree@ya.ru>
2013-01-05 21:38:13 +0400

    Gallery markup (1/2)

--- js/jquery/main.js
+++ js/jquery/main.js
@@ -11,7 +11,7 @@ $(document).ready(function()
  $('#ctrl-4').click(function() {$('.ctrl').css(ctrlUnCheck); $('#ctrl-4').css(ctrlCheck); $.scrollTo('#panel-4', 500, {axis:'y'}); $('title').text('Мероприятия | К16');});
  $('#ctrl-5').click(function() {$('.ctrl').css(ctrlUnCheck); $('#ctrl-5').css(ctrlCheck); $.scrollTo('#panel-5', 500, {axis:'y'}); $('title').text('Люди | К16');});
  $('#ctrl-6').click(function() {$('.ctrl').css(ctrlUnCheck); $('#ctrl-6').css(ctrlCheck); $.scrollTo('#panel-6', 500, {axis:'y'}); $('title').text('Ссылки | К16');});
- $('#ctrl-7').click(function() {$('.ctrl').css(ctrlUnCheck); $('#ctrl-7').css(ctrlCheck); $.scrollTo('#panel-7', 500, {axis:'y'}); $('title').text('Галерея | К16');});
+ $('#ctrl-7').click(function() {$('.ctrl').css(ctrlUnCheck); $('#ctrl-7').css(ctrlCheck); $.scrollTo('#panel-7', 500, {axis:'y'}); $('title').text('Фото | К16');});
  
  var ctrlCheck = 
  {
@@ -27,12 +27,9 @@ $(document).ready(function()
    'text-shadow' : '0px 0px 1px #fff',
  }
 
- $('#fpc').click(function() {$.scrollTo('#fp', 500, {axis:'x'});});
- $('#vpc').click(function() {$.scrollTo('#vp', 500, {axis:'x'});});
+ $('#ctrl-1').css(ctrlCheck);
  
- $('.back').click(function() {$.scrollTo(0, 500, {axis:'x'});});
- 
- $('#fback').click(function() {$.scrollTo('#f', 500, {axis:'x'});});
+ $('#gback, .back').click(function() {$.scrollTo(0, 500, {axis:'x'});});
   
  $('.g').fotorama();
 
@@ -45,11 +42,47 @@ $(document).ready(function()
  $('.down').mouseleave(function()      {$('.down').slideUp('fast').hide('fast');});
 
 
+
+// Dropdown array
+
+ $('#uc-1').click(function() {$.scrollTo('#u-1', 500, {axis:'x'});});
+ $('#uc-2').click(function() {$.scrollTo('#u-2', 500, {axis:'x'});});
+ $('#uc-3').click(function() {$.scrollTo('#u-3', 500, {axis:'x'});});
+ $('#uc-4').click(function() {$.scrollTo('#u-4', 500, {axis:'x'});});
+
+ $('#mc-1').click(function() {$.scrollTo('#m-1', 500, {axis:'x'});});
+ $('#mc-2').click(function() {$.scrollTo('#m-2', 500, {axis:'x'});});
+ $('#mc-3').click(function() {$.scrollTo('#m-3', 500, {axis:'x'});});
+ $('#mc-4').click(function() {$.scrollTo('#m-4', 500, {axis:'x'});});
+ $('#mc-5').click(function() {$.scrollTo('#m-5', 500, {axis:'x'});});
+
+ $('#lc-1').click(function() {$.scrollTo('#l-1', 500, {axis:'x'});});
+ $('#lc-2').click(function() {$.scrollTo('#l-2', 500, {axis:'x'});});
+ $('#lc-3').click(function() {$.scrollTo('#l-3', 500, {axis:'x'});});
+ $('#lc-4').click(function() {$.scrollTo('#l-4', 500, {axis:'x'});});
+
+
+// Gallery preview array
+
+ $('#gpc-1').click(function() {$.scrollTo('#gp-1', 500, {axis:'x'});});
+ $('#gpc-2').click(function() {$.scrollTo('#gp-2', 500, {axis:'x'});});
+ $('#gpc-3').click(function() {$.scrollTo('#gp-3', 500, {axis:'x'});});
+ $('#gpc-4').click(function() {$.scrollTo('#gp-4', 500, {axis:'x'});});
+ $('#gpc-5').click(function() {$.scrollTo('#gp-5', 500, {axis:'x'});});
+ $('#gpc-6').click(function() {$.scrollTo('#gp-6', 500, {axis:'x'});});
+ $('#gpc-7').click(function() {$.scrollTo('#gp-7', 500, {axis:'x'});});
+ $('#gpc-8').click(function() {$.scrollTo('#gp-8', 500, {axis:'x'});});
+ $('#gpc-9').click(function() {$.scrollTo('#gp-9', 500, {axis:'x'});});
+ $('#gpc-10').click(function() {$.scrollTo('#gp-10', 500, {axis:'x'});});
+ $('#gpc-11').click(function() {$.scrollTo('#gp-11', 500, {axis:'x'});});
+ $('#gpc-12').click(function() {$.scrollTo('#gp-12', 500, {axis:'x'});});
+
 // Smoothly animate array
 
- $('.gpa, .back, #fback, .ctrl, #gototop, .down A').removeAttr('href');
+ $('.gpa, .back, #gback, .fback, .ctrl, #gototop, .down A').removeAttr('href');
  
 //Hide menu for button 'Back' in galleries
+
  $(function () 
  {
 	$(window).scroll(function () 
@@ -65,7 +98,8 @@ $(document).ready(function()
 	});  
  });
 
- 
+ $('body').scrollTo(0, 500);
+
 });
 
 
