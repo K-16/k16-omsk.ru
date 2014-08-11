3a368ed6e24156a3b18b735c0dc95bf49bacca1e
upisfree <upisfree@outlook.com>
2014-08-12 05:16:02 +0700

    Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,92 @@
+module.exports = function(grunt)
+{
+  var jsPath  = 'client/js/',
+      cssPath = 'client/style/';
+
+  grunt.initConfig(
+  {
+    pkg: grunt.file.readJSON('package.json'),
+      concat:
+      {
+        build:
+        {
+          src:
+          [
+            jsPath + 'lib/jquery-*.js', // библиотеки
+            jsPath + 'lib/*.js', // библиотеки
+            jsPath + 'constants.js',
+            jsPath + 'config.js',
+            jsPath + 'templates.js',
+            jsPath + 'tools.js',
+            jsPath + 'debug.js',
+            jsPath + 'parsers.js',
+            jsPath + 'menu.js',
+            jsPath + 'system.js',
+            jsPath + 'start.js',
+            jsPath + 'elements.js'
+          ],
+          dest: jsPath + 'compiled.js'
+        }
+      },
+
+      less:
+      {
+        build:
+        {
+          files: 
+          {
+            'client/style/css/compiled.css':
+            [
+              cssPath + 'css/lib/*.css',
+              cssPath + 'less/main.less'
+            ]
+          },
+          options:
+          {
+            cleancss: true,
+            compress: true
+          }
+        }
+      },
+
+      uglify:
+      {
+        build:
+        {
+          src: jsPath + 'compiled.js',
+          dest: jsPath + 'compiled.js'
+        }
+      },
+
+      watch:
+      {
+        js:
+        {
+          files: [jsPath + '**/*.*', '!' + jsPath + 'compiled.js'],
+          tasks: ['concat', 'uglify']
+        },
+
+        less:
+        {
+          files: [cssPath + '**/*.*', '!' + cssPath + 'css/compiled.css'],
+          tasks: ['less']
+        },
+
+        gruntfile:
+        {
+          files: 'Gruntfile.js',
+          options:
+          {
+            reload: true
+          }
+        }
+      }
+    });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', 'watch');
+};
